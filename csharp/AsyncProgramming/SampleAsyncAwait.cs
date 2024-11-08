d35b48c3218acd753b55e9c6e4e79dcb6d3d695a
richanitdgp <81793293+richanitdgp@users.noreply.github.com>
2024-11-08 11:31:44 -0800

    Add files via upload

--- csharp/AsyncProgramming/SampleAsyncAwait.cs
+++ csharp/AsyncProgramming/SampleAsyncAwait.cs
@@ -6,6 +6,8 @@ namespace AsyncProgramming
 {
     public static class SampleAsyncAwait
     {
+        // Keyword async is used for async programming
+        // If a method contains await expressions, it should be marked as async
         public static async Task Main()
         {
             Console.WriteLine("Downloading content...");
@@ -20,6 +22,10 @@ namespace AsyncProgramming
         {
             using(HttpClient client = new HttpClient())
             {
+                // The caller thread pauses execution and the await expression
+                // gets executed within another thread from the thread pool.
+                // The caller thread resumes when result is obtained from await expression
+                // await is non-blocking for caller thread - good performance
                 string result = await client.GetStringAsync("https://microsoft.com");
                 return result;
             }
