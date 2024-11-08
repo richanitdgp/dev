f90eb284a4ddf696f0d71634ff972b774199bc18
richanitdgp <81793293+richanitdgp@users.noreply.github.com>
2024-11-08 11:28:26 -0800

    Add files via upload

---
+++ csharp/AsyncProgramming/SampleAsyncAwait.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace AsyncProgramming
+{
+    public static class SampleAsyncAwait
+    {
+        public static async Task Main()
+        {
+            Console.WriteLine("Downloading content...");
+
+            string content = await DownloadContentAsync();
+
+            Console.WriteLine("Download completed");
+            Console.WriteLine($"Content length: {content.Length}");
+        }
+
+        public static async Task<string> DownloadContentAsync()
+        {
+            using(HttpClient client = new HttpClient())
+            {
+                string result = await client.GetStringAsync("https://microsoft.com");
+                return result;
+            }
+        }
+    }
+}
