7e9d17b2ca8facd62202e349a4dc131b0f41cdd8
richanitdgp <81793293+richanitdgp@users.noreply.github.com>
2024-11-12 15:56:47 -0800

    Add files via upload

--- csharp/LRUCache/LRUCache.cs
+++ csharp/LRUCache/LRUCache.cs
@@ -10,11 +10,17 @@ namespace LRUCache
         private int count;
         public ConcurrentDictionary<int, LRUNode> cache;
         public LRUDoublyLinkedList lrulist;
+        ReaderWriterLockSlim rwlock = new ReaderWriterLockSlim();
 
         public LRUCache(int capacity)
         {
             this.capacity = capacity;
+            
+            rwlock.EnterWriteLock();
             this.count = 0;
+            Console.WriteLine("Initialized cache count=0");
+            rwlock.ExitWriteLock();
+            
             this.cache = new ConcurrentDictionary<int, LRUNode>(2, 10);
             this.lrulist = new LRUDoublyLinkedList();
         }
@@ -22,6 +28,8 @@ namespace LRUCache
         // Return the value for this key if present in the cache, -1 otherwise
         public int GetItem(int key)
         {
+            Console.WriteLine($"Fetching item for key '{key}' from cache");
+
             if (cache.ContainsKey(key))
             {
                 // add this item to the front of the LRU list
@@ -36,6 +44,27 @@ namespace LRUCache
         // Add an entry to save the current key, value pair in the cache
         public void AddItem(int key, int val)
         {
+            Console.WriteLine($"Adding item '{key},{val}' to cache");
+            rwlock.EnterReadLock();
+            // Check if cache has reached capacity
+            if (this.count == this.capacity)
+            {
+                Console.WriteLine("Cache count reached capacity - removing an item");
+                rwlock.ExitReadLock();
+                // Remove LRU item before adding new one
+                LRUNode remove = lrulist.RemoveItemFromTail();
+
+                // Remoce item from cache
+                KeyValuePair<int, LRUNode> kvp = new KeyValuePair<int, LRUNode>(key, remove);
+                cache.TryRemove(kvp);
+
+                rwlock.EnterWriteLock();
+                this.count--;
+                rwlock.ExitWriteLock();
+            }
+            else
+                rwlock.ExitReadLock();
+
             LRUNode node;
             if (!cache.ContainsKey(key))
             {
@@ -44,6 +73,12 @@ namespace LRUCache
 
                 // Add to top of LRU list
                 lrulist.AddItemToFront(node);
+
+                rwlock.EnterWriteLock();
+                this.count++;
+                Console.WriteLine($"Added iten with key '{key}' to cache and count={count}");
+                rwlock.ExitWriteLock();
+
             }
             else
             {
--- csharp/LRUCache/Program.cs
+++ csharp/LRUCache/Program.cs
@@ -11,10 +11,10 @@ namespace LRUCache
         public static void Main()
         {
             Console.WriteLine("Starting cache...");
-            LRUCache cache = new LRUCache(10);
+            LRUCache cache = new LRUCache(5);
 
-            Console.WriteLine("Adding item '5' to cache...");
-            cache.AddItem(5, 123);
+            Console.WriteLine("Adding item '8' to cache...");
+            cache.AddItem(8, 123);
             Console.WriteLine("Completed adding item '5' to cache...");
 
             Console.WriteLine("Fetching item '3'...");
@@ -37,6 +37,10 @@ namespace LRUCache
             }
 
             Task.WaitAll(tasks.ToArray());
+
+            Console.WriteLine("Fetching item '3'...");
+            val = cache.GetItem(3);
+            Console.WriteLine($"Fetched item with key '3' having value '{val}'");
 
         }
 
