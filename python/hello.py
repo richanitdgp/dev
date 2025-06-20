b4954d79bcfd5c52551e014c86372d0ff97e70af
richanitdgp <81793293+richanitdgp@users.noreply.github.com>
2025-06-19 19:55:44 -0700

    new strings

--- python/hello.py
+++ python/hello.py
@@ -9,8 +9,3 @@ print("b = ", b)
 print("Type of a is ", type(a), " Type of b is ", type(b))
 
 print("Happy\tFriday")
-
-s = "Happy Thursday"
-print(s)
-
-print(s[6:])
---
+++ python/strings.py
@@ -0,0 +1,19 @@
+s = "Happy Thursday"
+print(s)
+
+print(s[6:])
+print(s[:5])
+print(s[1:5]) #print substring from index 1 to 5
+
+print(s[::]) # print original string
+print(s[::2]) # print alternate chars
+print(s[::-1]) # reverse
+
+s1 = "Hello..."
+s2 = s1 + "it is beautiful outside!!"
+print(s2)
+
+print(s2.upper())
+print(s2.lower())
+print(s2.split())
+print(s2.split('i'))
