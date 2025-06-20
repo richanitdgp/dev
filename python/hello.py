e9dbe31acc387c288d65f8418384a28d74ccba22
richanitdgp <81793293+richanitdgp@users.noreply.github.com>
2025-06-19 19:40:21 -0700

    more logic

--- python/hello.py
+++ python/hello.py
@@ -5,3 +5,12 @@ print("a = ", a)
 
 b = 4 * 9
 print("b = ", b)
+
+print("Type of a is ", type(a), " Type of b is ", type(b))
+
+print("Happy\tFriday")
+
+s = "Happy Thursday"
+print(s)
+
+print(s[6:])
